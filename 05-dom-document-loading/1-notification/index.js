8dfd15413bb77f3dfc4e35dd35f95131384d3291
newsposting <48893873+newsposting@users.noreply.github.com>
2022-05-09 00:23:45 -0500

    Task #9 "NotificationMessage"

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,50 @@
 export default class NotificationMessage {
+  constructor(message = "", { duration = 0, type = "success" } = {}) {
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
+    this.createNotificationElement();
+  }
 
+  show(targetElement = document.body) {
+    this.remove();
+
+    targetElement.append(this.element);
+
+    if (this.duration > 0) 
+      setTimeout(this.remove, this.duration);
+  }
+
+  createNotificationElement() {
+    this.element = document.createElement("div");
+    this.element.classList.add("notification");
+    this.element.classList.add(this.type);
+    this.element.style = `--value:${this.duration / 1000}s`;
+    this.element.innerHTML = this.getMessageTemplate();
+  }
+
+  remove() {
+    let notificationElement = document.querySelector(".notification");
+    
+    if (notificationElement) 
+      notificationElement.remove();
+  }
+
+  destroy() {
+    this.message = null;
+    this.duration = null;
+    this.type = null;
+    this.remove();
+  }
+
+  getMessageTemplate() {
+    return `        
+      <div class="timer"></div>
+      <div class="inner-wrapper">
+        <div class="notification-header">${this.type}</div>
+        <div class="notification-body">
+          ${this.message}
+        </div>
+      </div>`;
+  }
 }
